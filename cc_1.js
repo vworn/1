0590332a59c58176e1e8731d2065a46550769982
vworn <jasonkmathewpersonal@gmail.com>
2025-01-23 17:33:41 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Variables
+let employeeName = "John Doe";
+const employeeID = 12345;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+console.log("Is Active:", isActive, "Type:", typeof isActive);
