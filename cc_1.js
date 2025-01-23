45da0c0d283969d8b1b00bb377ad96708cec4dd7
vworn <jasonkmathewpersonal@gmail.com>
2025-01-23 17:34:28 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
 console.log("Is Active:", isActive, "Type:", typeof isActive);
+// Task 2: Primitive Data Types
+let productName = "Wireless Headphones";
+const productPrice = 79.99;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
